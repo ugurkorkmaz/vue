c8e43fc00c352a4bfb1e1c161cdf02dc4537bf25
Uğur Korkmaz <40540244+ugurkorkmaz@users.noreply.github.com>
2020-09-19 16:36:27 +0300

    Add files via upload

---
+++ precache-manifest.ed6c4c740cc7b3de087be8a74643211f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0aaf90d3c6624acae8",
+    "url": "/css/about.ba110f97.css"
+  },
+  {
+    "revision": "266627e58bb1753776cb",
+    "url": "/css/app.ba014d55.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4df871f6e6e820374bba7bf2cf79d578",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c0aaf90d3c6624acae8",
+    "url": "/js/about.8e5ad866.js"
+  },
+  {
+    "revision": "266627e58bb1753776cb",
+    "url": "/js/app.27480e6b.js"
+  },
+  {
+    "revision": "7522c0720069a6f47515",
+    "url": "/js/chunk-vendors.ce061d87.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
